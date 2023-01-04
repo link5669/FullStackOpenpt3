46e1637ae3fd08ea57c8fd53752e56a9049d5c76
link5669 <milesacq@icloud.com>
2023-01-03 21:20:49 -0500

    3.1

---
+++ index.js
@@ -0,0 +1,41 @@
+var morgan = require('morgan')
+const express = require('express')
+morgan('tiny')
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World! Persons at <a href="/api/persons">this</a> link</h1>')
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
+const persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
